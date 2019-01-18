e5f66398bbc60b0e152c9dba3c0cc8ff556b36bb
TKMCode <tyler.code.maples@gmail.com>
2019-01-17 23:42:43 -0800

    finished omdb & spotify logic, need i want it that way

--- liri.js
+++ liri.js
@@ -1,17 +1,18 @@
+// storing all requires that will be used into their own variables
 var env = require("dotenv").config();
 var request = require('request');
-var nodeSpotify = require('node-spotify-api');
-var moment = require('moment');
+var Spotify = require('node-spotify-api');
 var fs = require('fs');
 var axios = require('axios');
-// is this right for the spotify????
-var spotify = require('./keys');
+var keys = require('./keys');
+// this one adds new contruct to the one that is passed from keys.spotify
+var spotify = new Spotify(keys.spotify);
 
-// method will be what they are trying to call
+// method will be what they are trying to call(omdb, spotify, dowhatitsays)
 var method = process.argv[2];
 
 // argument for what the user inputs
-var args = process.argv[3];
+var args = process.argv;
 
 // stores the user input and checks to see if the input has multiple words. 
 var userInput = "";
@@ -24,20 +25,23 @@ for(var i = 3; i < args.length; i++) {
   }
 }
 
-// functions
+// functions that will grab the information from the api's
+
 // omdb 
+
 var omdb = function(){
-    if (!args) {
-        args = 'Mr. Nobody';
+    // if the user does not input a movie, default to Mr. Nobody
+    if (args.length < 4) {
+        userInput = 'Mr. Nobody';
     }
+    console.log(userInput)
     axios.get(`http://www.omdbapi.com/?t=${userInput}&y=&plot=short&apikey=trilogy`)
     .then(function(response, data){
-        var movie = JSON.parse(response.data);
-        console.log(movie);
-        // Still need to change this part, but can't until axios returns the response
+        // store the response in a variable and then log each parameter. 
+        var movie = response.data
         console.log('-------------------------------------------------------');
         console.log(`Title:\t\t${movie.Title}`);
-        console.log(`IMDb Rating:\t${movie.Ratings[1].Value}`);
+        console.log(`IMDb Rating:\t${movie.Ratings[0].Value}`);
         console.log(`Country:\t${movie.Country}`);
         console.log(`Language:\t${movie.Language}`);
         console.log(`Plot:\t\t${movie.Plot}`);
@@ -50,34 +54,43 @@ var omdb = function(){
     })
     
 }
-// bands-in-town
-var concert = function(){
-    var bandUrl = `https://rest.bandsintown.com/artists/${userInput}/events?app_id=codingbootcamp`;
-    request(bandUrl, function(error, response, data) {
-        if (error) return console.log(error);
-        var bands = JSON.parse(data);
-        // add moment 
-        console.log("the bands are" + bands);
-        console.log('-------------------------------------------------------');
-        console.log('-------------------------------------------------------');
-    });
-} 
+
 // spotify
 
 var song = function(){
-    var songUrl = `https://api.spotify.com/v1/tracks/${userInput}`;
+    // if the user does not input a song, default to The Sign by Ace of Base.
+    if (args.length < 4){
+        userInput = 'The Sign';
+    }
+    spotify.search({ type: 'track', query: userInput }) 
+    .then(function(data) {
+        // store the response inside the songResp variable
+      var songResp = data.tracks.items;
+        // forEach loop through each response, pass the value of track, and get the info for each parameter.
+      songResp.forEach(function(track){
+        console.log('-------------------------------------------------------');
+        console.log(`Song:\t\t${track.name}`);
+        console.log(`Artist:\t\t${track.artists[0].name}`);
+        console.log(`Album:\t\t${track.album.name}`);
+        console.log(`Preview:\t${track.preview_url}`);
+        console.log('-------------------------------------------------------');
+      });
+    })
+    .catch(function(err){
+        console.log(err);
+    })
 
-}
+};
 
 // doWhatItSays
 
-// user input
-if (method === "movie-this") {
-   omdb();
+var says = function(){
+
 }
 
-else if (method === "concert-this") {
-   concert();
+// checks the method input, then runs the appropriate function
+if (method === "movie-this") {
+   omdb();
 }
 
 else if (method === "spotify-this-song") {
